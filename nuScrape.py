08e530e5ca0114faaef34e10f429fd7e7ed336cd
justinmiller33 <justinryanmiller@comcast.net>
2020-11-04 15:16:00 -0500

    Initializing nuScrape

---
+++ nuScrape.py
@@ -0,0 +1,7 @@
+# Scraping NUWorks employment portal using simulated webdriver
+# Justin Miller, Northeastern University COE
+# Paxton Howard, Northeastern University COS
+
+import numpy as np
+from selenium import webdriver
+
