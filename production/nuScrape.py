15f844eb7d60d1b5eda37ffa0e2f72c48619351a
justinmiller33 <justinryanmiller@comcast.net>
2020-11-04 17:49:09 -0500

    Finalizing scraper

--- production/nuScrape.py
+++ production/nuScrape.py
@@ -5,6 +5,8 @@
 # Modules
 import numpy as np
 from selenium import webdriver
+import time
+import pyautogui
 
 # -------
 # |USAGE|
@@ -12,19 +14,83 @@ from selenium import webdriver
 # Just a slightly jank simulated browser
 # --------------------------------------
 
+# Save html in a text file
+def saveTxtFile(postId,html):
+
+    txtName = postId + ".txt"
+    file = open("/home/justinmiller/devel/nuworks/rawFiles/" + txtName, "w")
+    f = file.write(html)
+    file.close()
 
 # Initializing Browser
 driver = webdriver.Chrome()
 
 # Link to search
-link = "https://northeastern-csm.symplicity.com/students/app/jobs/search?perPage=10000&job_type=5"
-
-# Loading up browser
+link = "https://northeastern-csm.symplicity.com/students/app/jobs/search?page=1&perPage=1&job_type=5"
+# Loading up nuworks
 driver.get(link)
+
+# Confirm login
+y = input("Confirm Login? y/n: ")
+# Don't really need a condition here
+
+# Going to nuworks search link
+driver.get(link)
+
+# Confirm load
+y = input("Confirm Page Load? y/n: ")
+# Don't really need a condition here
+
+# Get number of jobs in this load
+num = int(input("Number of Jobs: "))
 
 # Loading up body for calls
 body = driver.find_element_by_tag_name("body")
 
-def initPosition(body,driver):
+# Declaring the current job num
+jobNum = 1
+
+# Looping through each job
+for i in range(num):
+
+    # Opening Job Page
+    content = driver.find_element_by_css_selector("job-element.ng-isolate-scope")
+    content.click()
+
+    # While loading, sleep
+    while link == driver.current_url:
+        time.sleep(1)
+
+    # Save new url
+    newUrl = driver.current_url
+
+    # Get all html
+    html = driver.page_source
+
+    # Get post id from url
+    postId = newUrl[65:]
+
+    # Update
+    print("Iteration " + str(i))
+    print(postId)
+
+    # Save text file
+    saveTxtFile(postId,html)
+    
+    # Increasing jobnum
+    jobNum = jobNum + 1
+    # New link
+    link = "https://northeastern-csm.symplicity.com/students/app/jobs/search?page=" + str(jobNum) + "&perPage=1&job_type=5"
+
+    # Get next link
+    driver.get(link)
+
+    # While loading, sleep
+    while link == driver.current_url:
+        time.sleep(1)
+
+
+
+
 
 
